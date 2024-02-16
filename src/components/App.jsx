8f18c5851a350a6cffd91b57369d0dd0f30d393a
Евгения Додиван <evgenita.d97@gmail.com>
2024-02-16 16:10:12 +0100

    add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { ContactForm } from "./ContactForm/ContactForm";
+import { SearchBox } from "./SearchBox/SearchBox";
+import { ContactList } from "./ContactList/ContactList";
+import css from "./App.module.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const getInitialContacts = () => {
+  const savedContacts = window.localStorage.getItem('saved-contacts');
+  if (savedContacts !== null) {
+    return JSON.parse(savedContacts);
+  }
+
+    return     [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]
+};
+
+export const App = () => {
+  // Contacts
+  const [contacts, setContacts] = useState(getInitialContacts);
+
+  const onAddContact = (newContact) => {
+    setContacts(prevContacts => {
+      return [
+        ...prevContacts,
+        newContact
+      ];
+    });
+  };
+
+  const onDeleteContact = (id) => {
+    setContacts(prevContacts => {
+      return prevContacts.filter(contact => contact.id !== id);
+    });
+  };
+
+    useEffect(() => {
+    window.localStorage.setItem('saved-contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  // Filter
+  const [filter, setFilter] = useState("");
+  const onFilter = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase()));
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div>
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm onAdd={onAddContact}/>
+      <SearchBox value={filter} onChange={setFilter} />
+      <ContactList contacts={onFilter} onDelete={onDeleteContact} />
+    </div>
+  );
+};
